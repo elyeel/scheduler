b30d9f4c5e0c1432c06a7b06a705fb0fbd5d7f4b
elyeel <elyeel@yahoo.com>
2019-09-03 16:48:18 -0700

    done w/ listing interviewers, didn't work as it supposed to

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,22 +3,23 @@ import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss"
 
 export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={(event) => props.onChange(interviewer.id)}
+      />
+    );
+  });
   
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
-        {
-          props.interviewers.map(interviewer => (
-            <InterviewerListItem
-            key={interviewer.id}
-            name={interviewer.name}
-            avatar={interviewer.avatar}
-            selected={props.interviewer === interviewer.id}
-            setInterviewer={() => props.onChange(interviewer.id)}
-            />
-          ))
-        }
+        {interviewers}
       </ul>
     </section>
   )
