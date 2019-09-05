da93cdf3155c0e48d5e9c5d665af633c9cc22b1c
elyeel <elyeel@yahoo.com>
2019-09-05 15:13:25 -0700

    Finished all day 1

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,26 +3,30 @@ import "components/DayListItem.scss";
 import classname from "classnames";
 
 export default function DayListItem(props) {
-  const dayClass = classname(
-    "day-list__item",
-    {"day-list__item--selected": props.selected},
-    {"day-list__item--full": props.spots===0}
-  );
-  const formatSpots = function(spot) {
-    if (spot > 1) {
-      return `${spot} spots remaining`;
-    } else if (spot > 0) {
-      return `${spot} spot remaining`;
-    } else {
-      return `no spots remaining`;
-    }
-  };
-  const spt = formatSpots(props.spots);
+	const dayClass = classname(
+		"day-list__item",
+		{ "day-list__item--selected": props.selected },
+		{ "day-list__item--full": props.spots === 0 }
+	);
+	const formatSpots = function(spot) {
+		if (spot > 1) {
+			return `${spot} spots remaining`;
+		} else if (spot > 0) {
+			return `${spot} spot remaining`;
+		} else {
+			return `no spots remaining`;
+		}
+	};
+	const spt = formatSpots(props.spots);
 
-  return (
-    <li className={dayClass} onClick={() => props.setInterviewer(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{spt}</h3>
-    </li>
-  );
+	return (
+		<li
+			className={dayClass}
+			// key={props.key}
+			onClick={() => props.setInterviewer(props.name)}
+		>
+			<h2 className="text--regular">{props.name}</h2>
+			<h3 className="text--light">{spt}</h3>
+		</li>
+	);
 }
