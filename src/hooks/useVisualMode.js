1d435a9e5254f5e99c9a901ea46237314b10e38c
elyeel <elyeel@yahoo.com>
2019-09-24 21:18:37 -0700

    fixed error when db error mode on

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,31 +1,31 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
+	const [mode, setMode] = useState(initial);
+	const [history, setHistory] = useState([initial]);
 
-  const transition = function(mode, replace=false) {
-    if (replace) {
-      history.pop();
-      setHistory(history);
-      setMode(mode);
-      setHistory(prev => [...prev, mode]);
-    } else {
-      setMode(mode);
-      setHistory(prev => [...prev, mode]);
-    }
-  }
+	const transition = function(mode, replace = false) {
+		if (replace) {
+			history.pop();
+			setHistory(history);
+			setMode(mode);
+			setHistory(prev => [...prev, mode]);
+		} else {
+			setMode(mode);
+			setHistory(prev => [...prev, mode]);
+		}
+	};
 
-  const back = function(mode) {
-    if (history.length > 1) {
+	const back = function(mode) {
+		console.log(mode, history); //test props
+		if (history.length > 1) {
 			history.pop();
 			setHistory(history);
 			setMode(history.slice(-1)[0]);
 		} else {
-			setMode(history[0]);
+			setMode(mode); // error when database on error mode
 		}
-  }
+	};
 
-  return {mode, transition, back};
+	return { mode, transition, back };
 }
-
