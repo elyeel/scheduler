9e0974bb76f8241d347a525cd118cf725978522c
elyeel <elyeel@yahoo.com>
2019-10-23 23:54:40 -0700

    w8d1 mocking modules done

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,31 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, getByText } from "@testing-library/react";
 
 import Application from "components/Application";
+import Appointment from "components/Appointment";
+
+import {waitForElement, fireEvent} from "@testing-library/react";
+
+// const { container, debug } = render(<Application />);
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+    
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+})
+
+describe("Appointment", () => {
+  xit("always renders with create mode, never directly show mode", () => {
+    expect(getByText(Appointment, "Saving")).toBeInTheDocument();
+  })
 });
+
